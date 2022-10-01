0de121a47e477c43aaca95748904bc581815cf5a
kckuei <kevinkuei@gmail.com>
2022-09-30 19:46:14 -0700

    Initial start menu logic

--- lib/chess.rb
+++ lib/chess.rb
@@ -3,6 +3,7 @@
 require_relative './intro'
 require_relative './board'
 require_relative './logger'
+require 'set'
 
 # ChessGame class.
 class ChessGame
@@ -13,6 +14,8 @@ class ChessGame
     @intro = Intro.new
     @board = ChessBoard.new
     @log = Logger.new
+    @options = { move_suggest: true }
+    @goodbye = ['Hasta luego!', 'бакат!', 'Tschüß!', 'さようなら!', 'See ya next time!']
   end
 
   # Displays the game intro screen.
@@ -52,10 +55,118 @@ class ChessGame
     @board = ChessBoard.new
     @log = Logger.new
   end
+
+  def print_start_menu
+    puts "\nMake a selection:
+    \e[32m[1]\e[0m New game
+    \e[32m[2]\e[0m Load saved game
+    \e[32m[3]\e[0m How to play
+    \e[32m[4]\e[0m Options
+    \e[32m[5]\e[0m Exit
+    "
+  end
+
+  def print_gameloop_menu
+    puts "\nMake a selection:
+    \e[32m[1]\e[0m Enter a move
+    \e[32m[2]\e[0m Save game
+    \e[32m[3]\e[0m Quit
+    "
+  end
+
+  def print_how_to_play
+    puts "\e[33mChess - How to Play\e[0m
+    Check out these guides:
+    \e[33m[1]\e[0m https://www.chess.com/lessons/playing-the-game
+    \e[33m[2]\e[0m https://en.wikipedia.org/wiki/Rules_of_chess
+    "
+  end
+
+  def start_menu
+    valid = Set.new(1..5)
+    print_start_menu
+    input = gets.chomp.to_i
+    loop do
+      break if valid.include?(input)
+
+      print_start_menu
+      input = gets.chomp.to_i
+    end
+    eval_start_menu_selection(input)
+  end
+
+  def eval_start_menu_selection(input)
+    case input
+    when 1
+      new_game
+    when 2
+      # To be implemented...load saved state
+    when 3
+      print_how_to_play
+      start_menu
+    when 4
+      # To be implemented...options..maybe for color or scrambling pieces
+      # Maybe option to turn off automatic move suggestion?
+    when 5
+      puts @goodbye.sample(1)[0].yellow
+      exit
+    end
+  end
+
+  def gameloop_menu
+    valid = Set.new(1..3)
+    print_gameloop_menu
+    input = gets.chomp.to_i
+    loop do
+      break if valid.include?(input)
+
+      print_gameloop_menu
+      input = gets.chomp.to_i
+    end
+    eval_loop_menu_selection(input)
+  end
+
+  def eval_loop_menu_selection(input)
+    # Print player turn
+    case input
+    when 1
+      # Get current player move
+      #
+      # Pick a piece to move, e.g. 'b7'
+      # Validate the move
+      #
+    when 2
+      # Save the game, then...
+      puts "I should have saved your game but didn't because I haven't been implemented yet."
+      gameloop_menu
+    when 3
+      puts @goodbye.sample(1)[0].yellow
+      exit
+    end
+    # Toggle the active player
+  end
+
+  def new_game
+    setup
+    draw_board
+    gameloop_menu
+    # end game message
+    # view log, or any key to continue to new game
+    reset
+    start_menu
+  end
+
+  # Start the game
+  def start
+    intro_screen
+    start_menu
+  end
 end
 
-# Loop options:
-# [1] Enter a move
-# [2] List valid moves of a chess piece
-# [3] Save game
-# [4] Quit
+# Parse user input
+# Is it in the board? Is there a piece there? Does it belong to the player?
+# If not, query again.
+# If yes, get the valid moves, and print them.
+#   Query for next move
+#   Loop until valid move or to go back
+#   If go back flag, then
--- main.rb
+++ main.rb
@@ -2,52 +2,52 @@
 
 require_relative './lib/chess'
 
+# game = ChessGame.new
+# game.intro_screen
+# game.draw_board
+
+# game.setup
+# game.draw_board
+
+# queen2 = game.piece_at(:d7)
+# knight2 = game.piece_at(:b7)
+# pond1 = game.piece_at(:g1)
+# pond2 = game.piece_at(:g6)
+# game.force_move([7, 1], [5, 2])
+# game.force_move([0, 1], [2, 2])
+# game.force_move([6, 4], [4, 4])
+# game.force_move('e1', 'e3')
+# game.force_move('g7', 'f5')
+# game.force_move('g0', 'f2')
+# game.force_move('d7', 'd3')
+# game.force_move('g1', 'g4')
+# game.draw_board
+# queen2.print_valid_moves(queen2.find_next_valid_moves)
+# knight2.print_valid_moves(knight2.find_next_valid_moves)
+# pond1.print_valid_moves(pond1.find_next_valid_moves)
+# pond2.print_valid_moves(pond2.find_next_valid_moves)
+
+# game = ChessGame.new
+# game.setup
+# game.force_move([7, 1], [5, 2])
+# game.force_move([0, 1], [2, 2])
+# game.force_move([6, 4], [4, 4])
+# game.force_move('e1', 'e3')
+# game.force_move('g7', 'f5')
+# game.force_move('g0', 'f2')
+# game.force_move('d7', 'd3')
+# game.force_move('g1', 'g4')
+# game.force_move('e0', 'd7')
+# game.draw_board
+# puts "Player 2 check? #{game.board.check?(:p2)}"
+
+# game.force_move('d7', 'c7')
+# game.force_move('c7', 'a7')
+# game.force_move('c5', 'a4')
+# game.force_move('e7', 'c7')
+# game.draw_board
+# puts "Player 2 checkmate? #{game.board.checkmate?(:p2)}"
+
+
 game = ChessGame.new
-game.intro_screen
-game.draw_board
-
-game.setup
-game.draw_board
-
-queen2 = game.piece_at(:d7)
-knight2 = game.piece_at(:b7)
-pond1 = game.piece_at(:g1)
-pond2 = game.piece_at(:g6)
-game.force_move([7, 1], [5, 2])
-game.force_move([0, 1], [2, 2])
-game.force_move([6, 4], [4, 4])
-game.force_move('e1', 'e3')
-game.force_move('g7', 'f5')
-game.force_move('g0', 'f2')
-game.force_move('d7', 'd3')
-game.force_move('g1', 'g4')
-game.draw_board
-queen2.print_valid_moves(queen2.find_next_valid_moves)
-knight2.print_valid_moves(knight2.find_next_valid_moves)
-pond1.print_valid_moves(pond1.find_next_valid_moves)
-pond2.print_valid_moves(pond2.find_next_valid_moves)
-
-game = ChessGame.new
-game.setup
-queen2 = game.piece_at(:d7)
-knight2 = game.piece_at(:b7)
-pond1 = game.piece_at(:g1)
-pond2 = game.piece_at(:g6)
-game.force_move([7, 1], [5, 2])
-game.force_move([0, 1], [2, 2])
-game.force_move([6, 4], [4, 4])
-game.force_move('e1', 'e3')
-game.force_move('g7', 'f5')
-game.force_move('g0', 'f2')
-game.force_move('d7', 'd3')
-game.force_move('g1', 'g4')
-game.force_move('e0', 'd7')
-game.draw_board
-puts "Player 2 check? #{game.board.check?(:p2)}"
-
-game.force_move('d7', 'c7')
-game.force_move('c7', 'a7')
-game.force_move('c5', 'a4')
-game.force_move('e7', 'c7')
-game.draw_board
-puts "Player 2 checkmate? #{game.board.checkmate?(:p2)}"
+game.start
