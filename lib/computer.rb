ccb96a1402d2d6653ebd72a0a53562dc04595a30
kckuei <kevinkuei@gmail.com>
2022-10-04 19:41:03 -0700

    Add demo cases

--- lib/computer.rb
+++ lib/computer.rb
@@ -2,4 +2,16 @@
 
 # Computer class for computer component/AI scripts.
 # Not yet implemented.
-class Computer; end
+class Computer
+  # Can differentiate between aggressive/reckless vs. defense AI's.
+  # Maximize coverage on pieces vs. maximize killing potential.
+
+  # Methods
+  # Given a list of valid choices.
+  # Naively pick one as long as it is safe.
+
+  # Given a list of valid choices.
+  # Filter/rank by by moves that can capture a piece.
+  # Rank those moves by some cost to benefit metric.
+  # Randomly sample the top choices, when there are more than a handful to make it non-deterministic.
+end
